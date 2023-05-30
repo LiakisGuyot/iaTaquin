7a91dde0a58b91d17a8a1624ee491e28f7f965d4
hxpulse <mattadam01@yahoo.fr>
2023-05-30 15:13:55 +0200

    hhhh

---
+++ src/main/java/com/taquin/iataquin/Agent.java
@@ -0,0 +1,9 @@
+package com.taquin.iataquin;
+
+public class Agent {
+
+    public Case currentTile;
+    public Case goal;
+    public Grille currentGrid;
+
+}
---
+++ src/main/java/com/taquin/iataquin/Grille.java
@@ -0,0 +1,16 @@
+package com.taquin.iataquin;
+
+import java.util.ArrayList;
+
+public class Grille {
+
+    public int taille;
+
+    public ArrayList<Case> cases;
+
+    public ArrayList<Case> getNeighboors(Case c) {
+        ArrayList<Integer> coords = c.getCoords();
+        ArrayList<Case>
+    }
+
+}
