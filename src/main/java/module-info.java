3a3333c2296ee65dc17023bcfe29d6b30e9de4de
Benjamin Guyot de La Pommeraye <benjamin.guyot-de-la-pommeraye@etu.univ-lyon1.fr>
2023-05-30 14:56:11 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.taquin.iataquin {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.taquin.iataquin to javafx.fxml;
+    exports com.taquin.iataquin;
+}
