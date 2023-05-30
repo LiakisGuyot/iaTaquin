e153d41a95af3be743d62bf149e844a91724183d
hxpulse <mattadam01@yahoo.fr>
2023-05-30 15:33:01 +0200

    rrrrrrrr

--- src/main/java/com/taquin/iataquin/Grille.java
+++ src/main/java/com/taquin/iataquin/Grille.java
@@ -1,16 +1,36 @@
 package com.taquin.iataquin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Grille {
 
     public int taille;
 
-    public ArrayList<Case> cases;
+    public HashMap<Integer[], Case> cases;
 
+
+    public Case getCase(int x, int y){
+        Integer[] coords = {x, y};
+        return this.cases.get(coords);
+    }
     public ArrayList<Case> getNeighboors(Case c) {
         ArrayList<Integer> coords = c.getCoords();
-        ArrayList<Case> jjj
+        ArrayList<Case> res = new ArrayList<>();
+        if (coords.get(1) + 1 <= this.taille) {
+            res.add(this.getCase(coords.get(0), coords.get(1) + 1));
+        }
+        if (coords.get(1) - 1 >= 0) {
+            res.add(this.getCase(coords.get(0), coords.get(1) - 1));
+        }
+        if (coords.get(0) + 1 <= this.taille) {
+            res.add(this.getCase(coords.get(0) + 1, coords.get(1)));
+        }
+        if (coords.get(0) - 1 >= 0) {
+            res.add(this.getCase(coords.get(0) - 1, coords.get(1)));
+        }
+        return res;
     }
 
 }
